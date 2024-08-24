38dea1acd1caf87c2da8dc20b92a8630ed9282a1
ayodarian <L24550273@chihuahua2.tecnm.mx>
2024-08-23 21:56:44 -0600

    Add files via upload

---
+++ EVA1_2_Variables_CristianMaynez/src/main/java/com/mycompany/eva1_2_variables_cristianmaynez/EVA1_2_Variables_CristianMaynez.java
@@ -0,0 +1,52 @@
+./*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_2_variables_cristianmaynez;
+
+/**
+ *
+ * @author Administrator
+ */
+public class EVA1_2_Variables_CristianMaynez {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");//cadena de texto(noombre, numero de control)
+        System.out.println(100);//entero (Edad)
+        System.out.println('c');//caracter (Sexo)
+        System.out.println(3.1416);//numero flotante(salario)
+        System.out.println(true);//valores logicos(booleanos)
+        System.out.println(                                                    
+                                false
+        
+        );//fin de instruccion ";"
+        //Enteros
+        //int
+        //long
+        //short
+       
+        //Numeros con parte decimal:
+        //float
+        //double
+        
+        //Cadenas de texto:
+        //String
+        
+        //Caracters:
+        //char
+        
+        //Valores logicos:
+        //Boolean
+        
+        //Declaracion de variables:
+        //Sintaxis para declarar variables(REGLAS)
+        //Tipo de dato nombre de la variable
+        
+        String nombre;
+        String apellido;
+        String curp;
+        float promedio
+        
+      
+    }
+}
