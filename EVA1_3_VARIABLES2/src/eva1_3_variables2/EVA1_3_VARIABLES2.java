f9efbb20a42f6278c5214632be8d9b6e047867cd
ayodarian <L24550273@chihuahua2.tecnm.mx>
2024-08-27 11:30:01 -0600

    Add files via upload

---
+++ EVA1_3_VARIABLES2/src/eva1_3_variables2/EVA1_3_VARIABLES2.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_3_variables2;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_3_VARIABLES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //VEHICULOS:
+        String marca;
+        String modelo;
+        int year; //no le puse ñ por que en otros lenguajes no la aceptan
+        double precio; 
+        
+        //ASIGNACION
+        marca = "NISSAN";
+        modelo = "GTR";
+        year = 2008;
+        precio = 1000000.1;
+        
+        //Para imprimir los datos siempre con System.out.println
+        
+        System.out.println (marca);
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+        
+        
+        
+     
+    }
+    
+}
