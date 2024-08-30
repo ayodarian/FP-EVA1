3d740c606edadd2a5e58afba6b27d24885e2e439
ayodarian <L24550273@chihuahua2.tecnm.mx>
2024-08-29 20:52:29 -0600

    Add files via upload

---
+++ EVA1_4_TV/src/eva1_4_tv/EVA1_4_TV.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_4_tv;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_4_TV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String Marca;
+        String Modelo;
+        int tamaño;
+        String P;
+        double precio;
+        String tecnologia; //Estas son las variables
+        
+        Marca = "Samsung";
+        Modelo = "Pro";
+        tamaño = 60;
+        precio = 34999.99;
+        tecnologia = "Oled";
+        P = " Pulgadas";
+        
+        
+        System.out.println(Marca);
+        System.out.println(Modelo);
+        System.out.print(tamaño);
+        System.out.println(P);
+        System.out.println(precio);
+        System.out.println(tecnologia);
+        
+        
+                
+              
+               
+    }
+    
+}
