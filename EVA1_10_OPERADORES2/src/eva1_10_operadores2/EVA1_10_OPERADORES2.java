247ffc0a901250a94bda88d5964dc01adbbf0b69
ayodarian <L24550273@chihuahua2.tecnm.mx>
2024-09-04 00:02:54 -0600

    Add files via upload

---
+++ EVA1_10_OPERADORES2/src/eva1_10_operadores2/EVA1_10_OPERADORES2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_10_operadores2;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_10_OPERADORES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre = "Darian";
+        String apellido = "Maynez";
+        String nombComp;
+        //concatenar: unir cadenas de texto o dos variables
+        nombComp = nombre + " " + apellido;
+        System.out.println("Nombre completo del usuario: " + nombre);
+        System.out.println("Apellido del usuario: " + apellido);
+        System.out.println("Nombre completo del usuario:" + nombComp);
+        
+        
+    }
+    
+}
